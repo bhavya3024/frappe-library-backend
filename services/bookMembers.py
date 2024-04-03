30d38b7a65683179ee4772c0bb23599026b6af7e
Bhavya Dhiman <bhavyadhiman7@gmail.com>
2024-04-04 01:38:04 +0530

    add functionality for paying dues

--- services/bookMembers.py
+++ services/bookMembers.py
@@ -8,8 +8,9 @@ from typing import List, Dict
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import and_, func
 from connections.db import engine
-from fastapi.encoders import jsonable_encoder
 from utils.index import ResponseExecption
+from datetime import datetime
+
 
 SessionMaker = sessionmaker(bind=engine)
 
@@ -31,6 +32,7 @@ def create_book_member(book_member: BookMembersDto):
     if db_book.stock_amount < 1:
         session.close()
         raise ResponseExecption(status=400, message='This book is out of stock for now')
+    check_dues(book_member.member_id, session=session)
     difference = book_member.rent_end_date - book_member.rent_start_date
     if difference.days < 1:
         raise ResponseExecption(status=400, message="You need to rent a book for atleast one day")
@@ -50,45 +52,18 @@ def create_book_member(book_member: BookMembersDto):
     return db_book_member
 
 
-def get_book_member(page=1):
+def get_book_members(page=1):
     session = SessionMaker()
-    members = session.query(MembersModel).filter().limit(10).offset((page -1) * 10).all()
-    return members
+    book_member = session.query(BookMembersModel).filter().limit(10).offset((page -1) * 10).all()
+    return book_member
 
-
-def update_member(member=MembersDto, id=int):
+def pay_dues(id=int):
     session = SessionMaker()
-    db_member = session.query(MembersModel).filter_by(id=id).first()
-    if db_member is None:
-        raise ResponseExecption(status=404, message="Member not Found")
-    if member.first_name:
-        db_member.first_name = member.first_name
-    if member.middle_name:
-        db_member.middle_name = member.middle_name
-    if member.last_name:
-        db_member.last_name = member.last_name
-    if member.email:
-        another_member_with_same_email = session.query(MembersModel).filter(and_(MembersModel.id != db_member.id, MembersModel.email == member.email)).first()
-        if another_member_with_same_email:
-            session.close()
-            raise ResponseExecption(status=404, message="Another member having same email exists")
-        else: 
-            db_member.email = member.email
-    session.commit()
-    session.close()
-
-
-def get_member_by_id(id=int):
-    session = SessionMaker()
-    db_member = session.query(MembersModel).filter_by(id=id).first()
+    db_member = session.query(BookMembersModel).filter_by(id=id).first()
     session.close()
     if db_member is None:
         raise ResponseExecption(status=404, message="Member not Found")
-    return db_member
+    db_member.rent_paid = True
+    db_member.rent_paid_at = datetime.now()
 
-
-def delete_member(id=int):
-    session = SessionMaker()
-    session.query(MembersModel).delete(id=id)
-    session.commit()
 
